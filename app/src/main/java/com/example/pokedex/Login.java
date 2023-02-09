e00e799f51078e77d9b61850bc548115526d76c0
Ana <anacaroldolata@gmail.com>
2023-02-09 20:01:00 -0300

    auth

--- app/src/main/java/com/example/pokedex/Login.java
+++ app/src/main/java/com/example/pokedex/Login.java
@@ -3,14 +3,27 @@ package com.example.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.pokedex.apiPokemon.RetrofitConfigAuth;
+import com.example.pokedex.model.Usuario;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Login extends AppCompatActivity {
 
   EditText user, password;
+  AlertDialog alerta;
 
   @SuppressLint("MissingInflatedId")
   @Override
@@ -23,10 +36,32 @@ public class Login extends AppCompatActivity {
   }
 
   public void verify(View view){
-    //consultar BD
-    //ver se não tá vazio
-    Intent i = new Intent(Login.this, PesquisaTipoActivity.class);
-    startActivity(i);
-    finish();
+    if(user.length() == 0 || password.length() == 0){
+      Toast.makeText(this, "Digite usuário e senha", Toast.LENGTH_SHORT).show();
+    }else {
+      Usuario usuario = new Usuario();
+      usuario.setLogin(user.getText().toString());
+      usuario.setSenha(password.getText().toString());
+
+      Call<Usuario> call = new RetrofitConfigAuth().getAuthService().auth(usuario);
+
+      call.enqueue(new Callback<Usuario>() {
+        @Override
+        public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+          if(response.isSuccessful()) {
+            Intent i = new Intent(Login.this, PesquisaTipoActivity.class);
+            startActivity(i);
+            finish();
+          }else{
+            Toast.makeText(Login.this, "Usuário ou Senha incorretos", Toast.LENGTH_SHORT).show();
+          }
+        }
+
+        @Override
+        public void onFailure(Call<Usuario> call, Throwable t) {
+          Toast.makeText(Login.this, "Usuário ou Senha incorretos", Toast.LENGTH_SHORT).show();
+        }
+      });
+    }
   }
 }
