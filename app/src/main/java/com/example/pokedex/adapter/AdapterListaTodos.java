2ccc17dc473a772ac8f339635f6f6dfa3bcb01b9
Guilherme Janke <guilhermejanke15@gmail.com>
2023-02-19 21:15:49 -0300

    DEV - Imagem na base64

--- app/src/main/java/com/example/pokedex/adapter/AdapterListaTodos.java
+++ app/src/main/java/com/example/pokedex/adapter/AdapterListaTodos.java
@@ -1,5 +1,8 @@
 package com.example.pokedex.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pokedex.R;
 import com.example.pokedex.model.Pokemon;
+import com.google.gson.Gson;
 
 import java.util.List;
 
@@ -44,7 +48,13 @@ public class AdapterListaTodos extends RecyclerView.Adapter<AdapterListaTodos.My
   public void onBindViewHolder(@NonNull AdapterListaTodos.MyViewHolder holder, int position) {
     Pokemon obj = list.get(position);
     holder.name.setText(obj.getNome());
-    holder.image.setImageResource(R.drawable.img);
+
+    String jsonString = "{'image': 'base64-encoded-byte-array'}";
+    Gson gson = new Gson();
+    Pokemon myObject = gson.fromJson(jsonString, Pokemon.class);
+    byte[] img = Base64.decode(myObject.getFoto(), Base64.DEFAULT);
+    Bitmap bitmap = BitmapFactory.decodeByteArray(img,0,img.length);
+    holder.image.setImageBitmap(bitmap);
   }
 
   @Override
